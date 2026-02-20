47755ff21482c08c325829372de0b0fa6a4cf207
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2026-02-20 17:58:31 -0600

    basic signup page

---
+++ BiteBuddy-Client/src/components/pages/signup.tsx
@@ -0,0 +1,198 @@
+"use dom"
+
+import DOMProvider from "@/components/dom-provider"
+import { Label } from "@radix-ui/react-dropdown-menu"
+import { useState } from "react"
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native"
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
+import { Picker } from "@react-native-picker/picker"
+
+export default function Page() {
+    const [formState, setFormState] = useState({
+        email: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+        day: "",
+        month: "",
+        year: "",
+        major: "",
+        gender: "",
+    })
+
+    const handlePressed = () => {
+        console.log(formState)
+    }
+
+    const genderSelectOptions = [
+        { label: "Select your gender", value: "" },
+        { label: "Male", value: "male" },
+        { label: "Female", value: "female" },
+        { label: "Non-binary", value: "nonbinary" },
+        { label: "Prefer not to say", value: "na" },
+    ]
+
+    return (
+        <DOMProvider>
+            <SafeAreaProvider>
+                <SafeAreaView className="p-6 w-full max-w-[400px] mx-auto">
+                    <Text className="text-2xl font-bold mb-4">Sign up</Text>
+                    <View>
+                        <View className="mb-4">
+                            <Label className="text-base font-bold mb-2">
+                                Email
+                            </Label>
+                            <TextInput
+                                className="border border-gray-400 rounded-sm p-2"
+                                autoCapitalize="none"
+                                autoComplete="off"
+                                placeholder="Email"
+                                placeholderTextColor="#999"
+                                value={formState.email}
+                                onChangeText={(text) =>
+                                    setFormState({ ...formState, email: text })
+                                }
+                            />
+                        </View>
+                        <View className="mb-4">
+                            <Label className="text-base font-bold mb-2">Password</Label>
+                            <TextInput
+                                className="border border-gray-400 rounded-sm p-2"
+                                autoCapitalize="none"
+                                autoComplete="off"
+                                placeholder="Password"
+                                placeholderTextColor="#999"
+                                secureTextEntry
+                                value={formState.password}
+                                onChangeText={(text) =>
+                                    setFormState({ ...formState, password: text })
+                                }
+                            />
+                        </View>
+                        <View className="mb-4">
+                            <Label className="text-base font-bold mb-2">First Name</Label>
+                            <TextInput
+                                className="border border-gray-400 rounded-sm p-2"
+                                autoCapitalize="words"
+                                autoComplete="name"
+                                placeholder="First Name"
+                                placeholderTextColor="#999"
+                                value={formState.firstName}
+                                onChangeText={(text) =>
+                                    setFormState({ ...formState, firstName: text })
+                                }
+                            />
+                        </View>
+                        <View className="mb-4">
+                            <Label className="text-base font-bold mb-2">Last Name</Label>
+                            <TextInput
+                                className="border border-gray-400 rounded-sm p-2"
+                                autoCapitalize="words"
+                                autoComplete="name"
+                                placeholder="Last Name"
+                                placeholderTextColor="#999"
+                                value={formState.lastName}
+                                onChangeText={(text) =>
+                                    setFormState({ ...formState, lastName: text })
+                                }
+                            />
+                        </View>
+                    </View>
+                    <View>
+                        <View className="mb-4">
+                            <Label className="text-base font-bold mb-2">Birthday</Label>
+                            <View className="flex-row justify-between mb-4">
+                                <View>
+                                    <Text className="text-sm mb-1" >Day</Text>
+                                    <TextInput
+                                        className="max-w-20 border border-gray-400 rounded-sm p-2"
+                                        keyboardType="number-pad"
+                                        placeholder="01"
+                                        placeholderTextColor="#999"
+                                        value={formState.day}
+                                        onChangeText={(text) =>
+                                            setFormState({ ...formState, day: text })
+                                        }
+                                    />
+                                </View>
+                                <View>
+                                    <Text className="text-sm mb-1" >Month</Text>
+                                    <TextInput
+                                        className="max-w-20 border border-gray-400 rounded-sm p-2"
+                                        keyboardType="number-pad"
+                                        placeholder="01"
+                                        placeholderTextColor="#999"
+                                        value={formState.month}
+                                        onChangeText={(text) =>
+                                            setFormState({ ...formState, month: text })
+                                        }
+                                    />
+                                </View>
+                                <View>
+                                    <Text className="text-sm mb-1" >Year</Text>
+                                    <TextInput
+                                        className="max-w-20 border border-gray-400 rounded-sm p-2"
+                                        keyboardType="number-pad"
+                                        placeholder="2024"
+                                        placeholderTextColor="#999"
+                                        value={formState.year}
+                                        onChangeText={(text) =>
+                                            setFormState({ ...formState, year: text })
+                                        }
+                                    />
+                                </View>
+                            </View>
+                        </View>
+                        <View className="mb-4">
+                            <Label className="text-base font-bold mb-2">Major</Label>
+                            <TextInput
+                                className="border border-gray-400 rounded-sm p-2"
+                                autoCapitalize="words"
+                                autoComplete="off"
+                                placeholder="Major"
+                                placeholderTextColor="#999"
+                                value={formState.major}
+                                onChangeText={(text) =>
+                                    setFormState({ ...formState, major: text })
+                                }
+                            />
+                        </View>
+                        <View className="mb-4">
+                            <Label className="text-base font-bold mb-2">Gender</Label>
+                            <Picker
+                                className="border border-gray-400 rounded-sm p-2"
+                                selectedValue={formState.gender}
+                                onValueChange={(itemValue) =>
+                                    setFormState({ ...formState, gender: itemValue })
+                                }
+                            >
+                                {
+                                    genderSelectOptions.map((option) => (
+                                        <Picker.Item
+                                            key={option.value}
+                                            label={option.label}
+                                            value={option.value}
+                                        />
+                                    ))
+                                }
+                            </Picker>
+                        </View>
+                    </View>
+                    <Pressable
+                        className="bg-[hsla(4, 72%, 42%, 1)] mt-4 py-5 rounded-lg items-center"
+                        onPress={handlePressed}>
+                        <Text className="text-white text-base font-bold">Sign up</Text>
+                    </Pressable>
+                </SafeAreaView>
+            </SafeAreaProvider>
+        </DOMProvider>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 16,
+    },
+})
