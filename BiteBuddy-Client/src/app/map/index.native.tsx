697f73e9bfd1a0231ee41fa6aa6765657738c660
Jade Lee <jadelee0924@gmail.com>
2026-02-20 19:47:08 -0600

    separate map pages into web, mobile, and default

--- BiteBuddy-Client/src/app/map/index.native.tsx
+++ BiteBuddy-Client/src/app/map/index.native.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { PROVIDER_GOOGLE } from 'react-native-maps'
 import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function App() {
+export default function Map() {
   return (
     <View style={styles.container}>
-      <MapView provider={PROVIDER_GOOGLE} style={styles.map} />
+      <SafeAreaProvider>
+        <SafeAreaView>
+          <MapView provider={PROVIDER_GOOGLE} style={styles.map} />
+        </SafeAreaView>
+      </SafeAreaProvider>
     </View>
   );
 }
@@ -15,6 +20,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  
   map: {
     width: '100%',
     height: '100%',
